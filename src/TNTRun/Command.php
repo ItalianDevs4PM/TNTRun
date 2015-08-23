fd70ea444b1e1e74ac3907125832caf7e4694792
XEmAX32 <XEmAX32@gmail.com>
2015-08-23 11:57:51 +0200

    Update Command.php

--- src/TNTRun/Command.php
+++ src/TNTRun/Command.php
@@ -6,3 +6,10 @@ use pocketmine\utils\TextFormat;
 use pocketmine\plugin\PluginBase;
 use pocketmine\command\CommandExecutor;
 use pocketmine\command\CommandSender;
+
+class Command extends PluginBase{
+
+public function onCommand(CommandSender $sender, Command $cmd, $label, array $args){
+		switch($cmd->getName()){
+	case 
+}
