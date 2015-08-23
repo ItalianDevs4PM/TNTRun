5bb9687f65ca83e45a13012bbe05685de4bcc0fe
XEmAX32 <XEmAX32@gmail.com>
2015-08-23 11:56:44 +0200

    Create Command.php

---
+++ src/TNTRun/Command.php
@@ -0,0 +1,8 @@
+<?php
+
+namespace TNTRun/Command;
+
+use pocketmine\utils\TextFormat;
+use pocketmine\plugin\PluginBase;
+use pocketmine\command\CommandExecutor;
+use pocketmine\command\CommandSender;
