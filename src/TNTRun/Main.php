0806b41f15dc08735341667fa2d2b8805fd7b055
fycarman <manuelficara01@gmail.com>
2015-08-23 13:55:23 +0200

    Update Main.php

--- src/TNTRun/Main.php
+++ src/TNTRun/Main.php
@@ -4,10 +4,26 @@ namespace TNTRun/Main;
 
 use pocketmine\utils\TextFormat;
 use pocketmine\plugin\PluginBase;
+use pocketmine\event\Listener;
+use pocketmine\event\block\BlockBreakEvent;
+use pocketmine\level\Level;
+use pocketmine\Player;
+use pocketmine\utils\Config;
 
-class Main extends PluginBase{
+class Main extends PluginBase implements Listener{
 
-public function OnEnable(){
+public function onEnable(){
+$this->getServer()->getPluginManage()->registerEvents($this, $this);
+$this->getLogger()->info(TextFormat::GREEN . "TNTRun Enabled!");
 $this->saveDefaultConfig();
+}
+
+public function onLoad(){
+$this->getLogger->info(TextFormat::YELLOW . "Loading TNTRun...");
+}
+
+public function onDisable(){
+$this->getLogger()->info(TextFormat::RED . "TNTRun Disabled");
+$this->getConfig()->save();
 }
 }
