df2e559f7db7c089c41e98ad83cf2f5d20dc30ed
xionbig <nikolazatsa3000@gmail.com>
2015-08-23 14:55:28 +0200

    Added Licence, Modified the Command Class

--- src/TNTRun/Command.php
+++ src/TNTRun/Command.php
@@ -1,30 +1,42 @@
 <?php
 
-namespace TNTRun/Command;
+namespace TNTRun\Command;
 
 use pocketmine\utils\TextFormat;
-use pocketmine\plugin\PluginBase;
-use pocketmine\command\CommandExecutor;
+use pocketmine\command\Command;
 use pocketmine\command\CommandSender;
+use pocketmine\command\ConsoleCommandSender;
+use pocketmine\command\PluginIdentifiableCommand;
 
-class Command extends PluginBase{
+class Command extends Command implements PluginIdentifiableCommand{
+    private $main;
 
-public function onCommand(CommandSender $sender, Command $cmd, $label, array $args){
-		switch($cmd->getName()){
-	
-	$block = $this->getConfig()->get("block");
-	
-	case "tr help":
-	$sender->sendMessage(TextFormat::GREEN . "----Help For TnTRun----");
-	$sender->sendMessage(TextFormat::GREEN . "/tr help for help");
-	$sender->sendMessage(TextFormat::GREEN . "/tr stats for statistics");
-	$sender->sendMessage(TextFormat::GREEN . "/tr leave for exit the match");
-	$sender->sendMessage(TextFormat::GREEN . "/tr about info for TnTRun");
-	break;
-	
-	case "tr about":
-	$sender->sendMessage(TextFormat::GREEN . "----TnTRun About----");
-	$sender->sendMessage(TextFormat::GREEN . "For play click the sign");
-	$sender->sendMessage(TextFormat::GREEN . "Run on the ".$block);
-	$sender->sendMessage(TextFormat::GREEN . "Maked BY ItaDevs4PM");
+    public function __construct(Main $main){
+        parent::__construct("tr", "MiniGame", "/tr <command> <value>");
+        $this->main = $main;
+    }
+    
+    public function execute(CommandSender $sender, $label, array $args){    
+        if(!isset($args) || !isset($args[0])) 
+            $args[0] = "help";
+        
+        switch(strtolower($args[0])){
+            case "help":
+                $sender->sendMessage(TextFormat::GREEN . "----Help For TnTRun----");
+                $sender->sendMessage(TextFormat::GREEN . "/tr help for help");
+                $sender->sendMessage(TextFormat::GREEN . "/tr stats for statistics");
+                $sender->sendMessage(TextFormat::GREEN . "/tr leave for exit the match");
+                $sender->sendMessage(TextFormat::GREEN . "/tr about info for TnTRun");
+            return;
+
+            case "about":
+                $block = $this->getConfig()->get("block");
+                $sender->sendMessage(TextFormat::GREEN . "----TnTRun About----");
+                $sender->sendMessage(TextFormat::GREEN . "For play click the sign");
+                $sender->sendMessage(TextFormat::GREEN . "Run on the ".$block);
+                $sender->sendMessage(TextFormat::GREEN . "Maked BY ItaDevs4PM");
+            return;
+        }
+        $sender->sendMessage(TextFormat::RED . "Comando non esiste");
+    }
 }
