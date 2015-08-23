77e15ae9526e4099b0c73f6ba9bd1fe6bc757aea
XEmAX32 <XEmAX32@gmail.com>
2015-08-23 15:05:20 +0200

    Update Match.php

--- src/TNTRun/Match.php
+++ src/TNTRun/Match.php
@@ -9,5 +9,12 @@ global pongame1 = [];
 global pongame2 = [];
 class Match extends PluginBase{
 
-
+public function onPlayerInteract(PlayerInteract $e){
+$player = $e->getPlayer();
+$block = $e->getBlock()->getId();
+if($player->hasPermission("tntrun.signjoin") or $player->hasPermission("tntrun.*")){
+if($block == 63 or $block == 68){
+}
+}
+}
 }
