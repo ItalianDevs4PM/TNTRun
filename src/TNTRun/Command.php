3df18a04f80f72bb1b5141d9600eb746005d0737
XEmAX32 <XEmAX32@gmail.com>
2015-08-23 12:11:16 +0200

    Update Command.php

--- src/TNTRun/Command.php
+++ src/TNTRun/Command.php
@@ -11,5 +11,20 @@ class Command extends PluginBase{
 
 public function onCommand(CommandSender $sender, Command $cmd, $label, array $args){
 		switch($cmd->getName()){
-	case 
+	
+	$block = $this->getConfig()->get("block");
+	
+	case "tr help":
+	$sender->sendMessage(TextFormat::GREEN . "----Help For TnTRun----");
+	$sender->sendMessage(TextFormat::GREEN . "/tr help for help");
+	$sender->sendMessage(TextFormat::GREEN . "/tr stats for statistics");
+	$sender->sendMessage(TextFormat::GREEN . "/tr leave for exit the match");
+	$sender->sendMessage(TextFormat::GREEN . "/tr about info for TnTRun");
+	break;
+	
+	case "tr about":
+	$sender->sendMessage(TextFormat::GREEN . "----TnTRun About----");
+	$sender->sendMessage(TextFormat::GREEN . "For play click the sign");
+	$sender->sendMessage(TextFormat::GREEN . "Run on the ".$block);
+	$sender->sendMessage(TextFormat::GREEN . "Maked BY ItaDevs4PM");
 }
