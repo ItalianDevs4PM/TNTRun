44e8051b6d69b1973db545b2c2b2f23a2e54b99a
XEmAX32 <XEmAX32@gmail.com>
2015-08-23 12:17:28 +0200

    Update Main.php

--- src/TNTRun/Main.php
+++ src/TNTRun/Main.php
@@ -1,1 +1,13 @@
+<?php
 
+namespace TNTRun/Main;
+
+use pocketmine\utils\TextFormat;
+use pocketmine\plugin\PluginBase;
+
+class Main extends PluginBase{
+
+public function OnEnable(){
+$this->saveDefaultConfig();
+}
+}
