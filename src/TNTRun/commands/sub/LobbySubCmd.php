39711290a21a4f85862ed70510b11ec93ffe1cc5
fycarman <manuelficara01@gmail.com>
2015-08-25 17:59:29 +0200

    Added TextFormat

--- src/TNTRun/commands/sub/LobbySubCmd.php
+++ src/TNTRun/commands/sub/LobbySubCmd.php
@@ -5,20 +5,21 @@ namespace TNTRun\commands\sub;
 use pocketmine\command\CommandSender;
 use pocketmine\Player;
 use TNTRun\commands\SubCmd;
+use pocketmine\utils\TextFormat;
 
 class LobbySubCmd extends SubCmd{
 
     public function execute(CommandSender $sender, array $args){
         if(!($sender instanceof Player)){
-            $sender->sendMessage("Please run this command in game");
+            $sender->sendMessage(TextFormat::YELLOW . "Please run this command in game!");
             return true;
         }
         $level = $this->getMain()->getServer()->getLevelByName($this->getMain()->getConfig()->get("lobby"));
         if($level !== null){
             $sender->teleport($level->getSafeSpawn());
-            $sender->sendMessage("Teleported to lobby");
+            $sender->sendMessage(TextFormat::GREEN . "Teleported to lobby");
         }else{
-            $sender->sendMessage("Lobby level isn't loaded or doesn't exist");
+            $sender->sendMessage(TextFormat::RED . "Lobby level isn't loaded or doesn't exist");
         }
         return true;
     }
