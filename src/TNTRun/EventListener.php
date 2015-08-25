0c95cb44d8168c12f38216dc818ed13374c00cd2
xionbig <nikolazatsa3000@gmail.com>
2015-08-25 20:07:24 +0200

    Update event

--- src/TNTRun/EventListener.php
+++ src/TNTRun/EventListener.php
@@ -12,9 +12,12 @@ use pocketmine\tile\Sign;
 use pocketmine\utils\TextFormat;
 use pocketmine\event\block\BlockBreakEvent;
 use TNTRun\tasks\UnsetBlockTask;
+use pocketmine\event\player\PlayerDeathEvent;
+use pocketmine\event\player\PlayerQuitEvent;
+use pocketmine\event\player\PlayerKickEvent;
 
 class EventListener implements Listener{
-
+    /** @var type */
     private $tntRun;
 
     public function __construct(Main $tntRun){
@@ -34,7 +37,7 @@ class EventListener implements Listener{
     
     public function onMove(PlayerMoveEvent $event){
         foreach($this->tntRun->arenas as $arena){
-            if($arena->getPlayerManager()->isPlaying($event->getPlayer()) and $arena->getStatusManager()->isRunning()){
+            if($arena->getPlayerManager()->isPlaying($event->getPlayer()) && $arena->getStatusManager()->isRunning()){
                 $p = $event->getPlayer();
                 $p->getServer()->getScheduler()->scheduleDelayedTask(new UnsetBlockTask($this->tntRun, $p->getLevel()->getBlock(new Vector3($p->getFloorX(), $p->getFloorY() - 1, $p->getFloorZ()))), 40);
                 if($p->y < $arena->getStructureManager()->getLowestFloorY()){
@@ -45,21 +48,51 @@ class EventListener implements Listener{
         }
     }
     
-    public function onBreak(BlockBreakEvent $event){
+    public function onDeath(PlayerDeathEvent $event){
         foreach($this->tntRun->arenas as $arena){
             if($arena->getPlayerManager()->isPlaying($event->getPlayer())){
+                $arena->getPlayerHandler()->leavePlayer($event->getPlayer());
+                return;
+            }
+        }
+    }
+    
+    public function onBreak(BlockBreakEvent $event){
+        foreach($this->tntRun->arenas as $arena){
+            if($arena->getPlayerManager()->isInArena($event->getPlayer())){
                 $event->setCancelled();
                 return;
             }
         }
     }
+    
     public function onPlace(BlockPlaceEvent $event){
         foreach($this->tntRun->arenas as $arena){
-            if($arena->getPlayerManager()->isPlaying($event->getPlayer())){
+            if($arena->getPlayerManager()->isInArena($event->getPlayer())){
                 $event->setCancelled();
                 return;
             }
         }
+    }
+    
+    public function onQuit(PlayerQuitEvent $event){
+        foreach($this->tntRun->arenas as $arena){
+            if($arena->getPlayerManager()->isInArena($event->getPlayer())){
+                $arena->getPlayerManager()->removePlayer($event->getPlayer());
+                $arena->getPlayerManager()->removeSpectator($event->getPlayer());
+                return;
+            }
+        }        
+    }
+    
+    public function onKick(PlayerKickEvent $event){
+        foreach($this->tntRun->arenas as $arena){
+            if($arena->getPlayerManager()->isInArena($event->getPlayer())){
+                $arena->getPlayerManager()->removePlayer($event->getPlayer());
+                $arena->getPlayerManager()->removeSpectator($event->getPlayer());
+                return;
+            }
+        }  
     }
 
 }
