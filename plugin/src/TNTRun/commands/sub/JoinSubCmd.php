ac4eb2c738d5ca64213279e44767084bfc450751
Luca P. <lucapetrucci28@yahoo.it>
2015-08-26 20:53:04 +0200

    Fixed join

--- plugin/src/TNTRun/commands/sub/JoinSubCmd.php
+++ plugin/src/TNTRun/commands/sub/JoinSubCmd.php
@@ -8,25 +8,36 @@ use pocketmine\Player;
 use TNTRun\commands\SubCmd;
 
 class JoinSubCmd extends SubCmd{
-    private $tntRun;
 
     public function execute(CommandSender $sender, array $args){
         if(!($sender instanceof Player)){
             $sender->sendMessage(TextFormat::YELLOW . "Please run this command in game!");
             return true;
         }
+        if(!isset($args[0])){
+            $sender->sendMessage(TextFormat::RED . "Please specify a valid arena name!");
+            return true;
+        }
+        if(!isset($this->getMain()->arenas[strtolower($args[0])])){
+            $sender->sendMessage(TextFormat::RED . "The arena ".$args[0]." does not exist");
+            return true;
+        }
+        $inGame = false;
         foreach($this->getMain()->arenas as $arena){
-        if(!isset($args[0])) {
-            $sender->sendMessage(TextFormat::YELLOW . "Please specify a valid arena name!");
-             }
-            if($arena->getPlayerManager()->isInArena($sender) && $arena->getPlayerManager()->isPlaying($sender)){
-                $sender->sendMessage(TextFormat::RED . "You are already in arena!");
-            }else{
-                $sender->teleport($this->tntRun->arenas[$args[0]]);
-                $arena->getPlayerHandler()->spawnPlayer($sender);
-                return true;
+            if($arena->getPlayerManager()->isPlaying($sender)){
+                $inGame = true;
+                break;
             }
         }
+        if($inGame){
+            $sender->sendMessage(TextFormat::RED . "Please quit/finish this game first");
+            return true;
+        }
+        if($this->getMain()->arenas[strtolower($args[0])]->getPlayerManager()->isPlaying($sender)){
+            $sender->sendMessage(TextFormat::RED . "You are already playing in that arena");
+            return true;
+        }
+        $this->getMain()->arenas[strtolower($args[0])]->getPlayerHandler()->spawnPlayer($sender);
         return true;
     }
 }
