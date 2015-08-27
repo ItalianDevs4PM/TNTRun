49b053ca75ebccae3891e3031967d00884d45849
Flavius12 <info@evolsoft.tk>
2015-08-27 14:20:14 +0200

    Fixed typo

--- plugin/src/TNTRun/commands/sub/InfoSubCmd.php
+++ plugin/src/TNTRun/commands/sub/InfoSubCmd.php
@@ -11,7 +11,7 @@ class InfoSubCmd extends SubCmd{
     public function execute(CommandSender $sender, array $args){
         $sender->sendMessage(TextFormat::GREEN."----TnTRun About----");
         $sender->sendMessage(TextFormat::GREEN."To play click the sign");
-        $sender->sendMessage(TextFormat::GREEN."Developed BY ItaDevs4PM");
+        $sender->sendMessage(TextFormat::GREEN."Developed BY ItalianDevs4PM");
         return true;
     }
 
