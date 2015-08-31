f24ed5de5cc8e110030abf7de2fdcf9a018418c2
xionbig <nikolazatsa3000@gmail.com>
2015-08-31 11:58:43 +0200

    Added reward command

---
+++ plugin/src/TNTRun/commands/sub/RewardSubCmd.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace TNTRun\commands\sub;
+
+
+use pocketmine\command\CommandSender;
+use pocketmine\utils\TextFormat;
+use TNTRun\commands\SubCmd;
+
+class RewardSubCmd extends SubCmd{
+
+    public function execute(CommandSender $sender, array $args){
+        if(!isset($args[0]) || !is_numeric($args[0])){
+            $sender->sendMessage(TextFormat::RED . "Please specify a valid value of money!");
+            return true;
+        }
+        $this->getMain()->getConfig()->set("money-reward", $args[0]);
+        $this->getMain()->getConfig()->save();
+        $sender->sendMessage(TextFormat::GREEN ."The award in money is now ".$args[0]);
+    }
+}
